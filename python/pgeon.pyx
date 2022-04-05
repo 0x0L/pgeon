c2007880b469b0ccf80ce039c56065767d034c4f
nullptr <3621629+0x0L@users.noreply.github.com>
2022-04-05 19:52:13 +0200

    working to pandas: issue with library versions conflicting

--- python/pgeon.pyx
+++ python/pgeon.pyx
@@ -3,12 +3,18 @@
 from libcpp.memory cimport shared_ptr
 from pyarrow.lib cimport import_pyarrow, CTable
 from pyarrow cimport wrap_table
+from pyarrow.lib import tobytes
 
 import_pyarrow()
 
 cdef extern from "../src/sql.h" namespace "pgeon":
-    cdef shared_ptr[CTable] GetTable(const char* conninfo, const char* query)
+    cdef shared_ptr[CTable] GetTable(const char* conninfo, const char* query) nogil
 
 def Getoo(conninfo, query):
-    cdef shared_ptr[CTable] tbl = GetTable(conninfo, query)
+    cdef:
+        shared_ptr[CTable] tbl
+
+    # with nogil:
+    tbl = GetTable(conninfo, query)
+
     return wrap_table(tbl)
