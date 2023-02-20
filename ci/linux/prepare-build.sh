48a01ca23c133a5722f44989eaba0e7c75e6cdef
nullptr <3621629+0x0L@users.noreply.github.com>
2023-02-20 16:45:33 +0100

    ci 3

--- ci/linux/prepare-build.sh
+++ ci/linux/prepare-build.sh
@@ -1,5 +1,6 @@
 #/usr/bin/env bash
 set -e
-python3 ignore-old-postgresql.py
+
+# python3 ignore-old-postgresql.py
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 yum install -y postgresql11-devel
