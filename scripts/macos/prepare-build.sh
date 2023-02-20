ed2df376e2c6dfa3befafb3b976aa7f2d5c31114
Ruben Di Battista <rubendibattista@gmail.com>
2023-02-20 01:12:18 +0100

    :wrench: chore: Hack around DYLD_LIBRARY_PATH

--- scripts/macos/prepare-build.sh
+++ scripts/macos/prepare-build.sh
@@ -1,5 +1,11 @@
 #/usr/bin/env bash
 set -e
 
-brew install apache-arrow apache-arrow-glib libpq
+if command -v port &> /dev/null
+then 
+    sudo port install apache-arrow postgresql11
+else
+    brew install apache-arrow apache-arrow-glib libpq
+fi
+
 python3 -m pip install pyarrow
--- scripts/macos/repair-wheel.sh
+++ scripts/macos/repair-wheel.sh
@@ -3,12 +3,31 @@ set -e
 
 echo "delocate wrapper script"
 
-export DYLD_LIBRARY_PATH=$(python -c 'import pyarrow as pa; print(":".join(pa.get_library_dirs()))')
+pyarrow_dir=$(python -c 'import pyarrow as pa; print(pa.get_library_dirs()[0])')
+pyarrow_version=$(python -c "import pyarrow as pa; print(pa.__version__.replace('.', ''))")
+
+echo "${pyarrow_version}"
+
+# Add a symlink to allow delocate-wheel to find libarrow_python
+DYLIB_HACK_DIR="$PWD/_skbuild/pyarrow_hack"
+mkdir $DYLIB_HACK_DIR
+ln -s "${pyarrow_dir}/libarrow_python.dylib" $DYLIB_HACK_DIR
+# ln -s "${pyarrow_dir}/libarrow.${pyarrow_version}.dylib" $DYLIB_HACK_DIR
+
+# Debug
+ls -al $DYLIB_HACK_DIR
+ls -al ${pyarrow_dir}
+
+export DYLD_LIBRARY_PATH=$DYLIB_HACK_DIR
 
 args=("$@")
 dest_dir=${args[0]}
 wheel=${args[1]}
 delocate_arch=${args[2]}
 
-delocate-wheel --require-archs {delocate_archs} -w {dest_dir} -v {wheel}
-rm -rf /project/_skbuild
+echo "dest_dir: ${dest_dir}"
+echo "wheel: ${wheel}"
+echo "delocate_arch: ${delocate_arch}"
+
+delocate-wheel --require-archs ${delocate_arch} -w ${dest_dir} -v ${wheel}
+rm -rf "$PWD/_skbuild"
