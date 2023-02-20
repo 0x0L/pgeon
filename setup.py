2b7dd9f8ba25bd99d2db637eb2641d9aa6050d10
nullptr <3621629+0x0L@users.noreply.github.com>
2023-02-20 19:55:04 +0100

    build pg_config in path in ci

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# import os
 import subprocess
 import sys
 from pathlib import Path
@@ -11,12 +12,13 @@ include_dir = "include"
 source_dir = "src"
 source_files = [str(p) for p in Path(source_dir).glob("**/*.cc")]
 
+pg_include = []
+pg_libdir = []
 try:
-    pg_include = subprocess.check_output(['pg_config', '--includedir']).decode().strip()
-    pg_libdir = subprocess.check_output(['pg_config', '--libdir']).decode().strip()
+    pg_include = [subprocess.check_output(['pg_config', '--includedir']).decode().strip()]
+    pg_libdir = [subprocess.check_output(['pg_config', '--libdir']).decode().strip()]
 except:
-    pg_include = subprocess.check_output(['pkg-config', '--cflags-only-I', 'libpq']).decode().strip()[2:]
-    pg_libdir = subprocess.check_output(['pkg-config', '--libs-only-L', 'libpq']).decode().strip()[2:]
+    pass
 
 extra_compile_args = ["-std=c++17"]
 if sys.platform == "darwin":
@@ -29,7 +31,7 @@ ext_libraries = [
         "pgeon_cpp",
         {
             "sources": source_files,
-            "include_dirs": [include_dir, source_dir, pa.get_include(), pg_include],
+            "include_dirs": [include_dir, source_dir, pa.get_include()] + [pg_include],
             "language": "c++",
             "macros": [("_GLIBCXX_USE_CXX11_ABI", "0")],
             "cflags": extra_compile_args,
@@ -44,7 +46,7 @@ extensions = [
         language="c++",
         include_dirs=[include_dir, np.get_include(), pa.get_include()],
         libraries=["pgeon_cpp", "pq"] + pa.get_libraries(),
-        library_dirs=[pg_libdir] + pa.get_library_dirs(),
+        library_dirs=pg_libdir + pa.get_library_dirs(),
         extra_compile_args=extra_compile_args,
     )
 ]
