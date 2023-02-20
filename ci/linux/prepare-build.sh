386f8efa51f50bafad393b4a969a139994e4de80
nullptr <3621629+0x0L@users.noreply.github.com>
2023-02-20 20:09:06 +0100

    again

--- ci/linux/prepare-build.sh
+++ ci/linux/prepare-build.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 set -e
 
-yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql11-devel
+yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm || true
+yum install -y postgresql11-devel || true
 
 ln -sf /usr/pgsql-11/bin/pg_config /usr/bin/
