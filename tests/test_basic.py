84e622c09e59ffdb1bee473fe29ecec8251d2321
Ruben Di Battista <rubendibattista@gmail.com>
2023-02-27 00:21:10 +0100

    :white_check_mark: test: Add xfail for windows

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,10 @@
+import sys
+
 import pyarrow as pa
 import pyarrow.compute as pc
 import pytest
 
 from pgeon import copy_query
-
 
 u = pa.array([-3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5])
 u_int = pc.round(u, round_mode="towards_infinity")
@@ -13,8 +14,9 @@ tests = [
         "SELECT * FROM (VALUES (True), (False)) AS foo",
         pa.table({"column1": [True, False]}),
     ),
-    (
-        """
+    pytest.param(
+        (
+            """
 SELECT
     x::smallint AS int16,
     x::integer AS int32,
@@ -25,21 +27,26 @@ SELECT
     x::numeric AS numeric_default
 FROM generate_series(-3.5, 3.5, 1) AS x
 """,
-        pa.table(
-            {
-                "int16": u_int.cast(pa.int16()),
-                "int32": u_int.cast(pa.int32()),
-                "int64": u_int.cast(pa.int64()),
-                "float": u.cast(pa.float32()),
-                "double": u,
-                "numeric_13_3": u.cast(pa.decimal128(13, 3)),
-                "numeric_default": u.cast(pa.decimal128(22, 6)),
-            }
+            pa.table(
+                {
+                    "int16": u_int.cast(pa.int16()),
+                    "int32": u_int.cast(pa.int32()),
+                    "int64": u_int.cast(pa.int64()),
+                    "float": u.cast(pa.float32()),
+                    "double": u,
+                    "numeric_13_3": u.cast(pa.decimal128(13, 3)),
+                    "numeric_default": u.cast(pa.decimal128(22, 6)),
+                }
+            ),
         ),
+        marks=pytest.mark.xfail(sys.platform == "win32", reason="Bug on Windows"),
     ),
-    (
-        "SELECT '12.3342'::money",
-        pa.table({"money": pa.array([12.33]).cast(pa.decimal128(22, 2))}),
+    pytest.param(
+        (
+            "SELECT '12.3342'::money",
+            pa.table({"money": pa.array([12.33]).cast(pa.decimal128(22, 2))}),
+        ),
+        marks=pytest.mark.xfail(sys.platform == "win32", reason="Bug on Windows"),
     ),
     ("SELECT 'ok'::character(4)", pa.table({"bpchar": pa.array(["ok  "])})),
     ("SELECT 'too long'::varchar(5)", pa.table({"varchar": pa.array(["too l"])})),
