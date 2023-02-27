0398637e2c6bf5e3f385c024fb34e386c97458b5
nullptr <3621629+0x0L@users.noreply.github.com>
2023-02-27 13:47:33 +0100

    bash syntax

--- ci/setup-db.sh
+++ ci/setup-db.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 set -e
 
-if [ "$RUNNER_OS" == "Linux" ];
+if [ "$RUNNER_OS" == "Linux" ]; then
 echo "Install postgres"
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm || true
 yum install -y postgresql11-devel || true
